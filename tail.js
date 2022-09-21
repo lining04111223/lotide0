eba9ed9b1a8e4dcb2b380051e6c4582a474f7d17
Ning Li <lining04111223@gmail.com>
2022-09-21 04:28:14 +0000

    head,tail

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array){
+  return array.slice(1);
+}
+
+// TEST CODE
+const words = ["Hello", "Lighthouse", "Labs"];
+const result = tail(words);
+assertEqual(words.length, 3); // ensure original array should still have 3 elements!
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
