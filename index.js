09e9486ab8096164ca3aca018150e80ec0a8d472
Ning Li <lining04111223@gmail.com>
2022-09-27 04:19:01 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head:      head,
+  tail:      tail,
+  middle:    middle,
+  eqArrays:  eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+};
